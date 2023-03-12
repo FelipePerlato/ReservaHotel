e7c75353d7a3821a9f1a49742bc453899506af5f
Felipe Perlato <fe_perlato@hotmail.com>
2023-03-12 19:04:07 -0300

    primeiro commit

---
+++ Reservahotel/Classes/Reservas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservahotel.Classes
+{
+    internal class Reservas
+    {
+        public int Numquarto { get; set; }
+
+        public DateTime Dataentrada { get; set; }
+
+        public DateTime Datasaida { get; set; }
+
+        public Reservas()
+        {
+
+        }
+        public Reservas(int numquarto, DateTime dataentrada, DateTime datasaida)
+        {
+            Numquarto = numquarto;
+            Dataentrada = dataentrada;
+            Datasaida = datasaida;
+        }
+
+        public int Duracao()
+        {
+            TimeSpan duracao = Datasaida.Subtract(Dataentrada);
+
+            return (int)duracao.TotalDays;
+        }
+
+        public override string ToString()
+        {
+            return "Numquarto "
+                + Numquarto
+                 + "dataentrada "
+                  + Dataentrada.ToString("dd/MM/yyyy")
+                   + " datasaida "
+                    + Datasaida.ToString("dd/MM/yyyy")
+                     + Duracao()
+                      + " noites";
+
+        }
+
+
+    }
+}
---
+++ Reservahotel/Program.cs
@@ -0,0 +1,31 @@
+﻿using Reservahotel.Classes;
+using System;
+
+internal class Program
+{
+   public static void Main(string[] args)
+    {
+        List<Reservas> lista = new List<Reservas>(); 
+
+        Console.WriteLine("Numero da sala ");
+        int numquarto = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Data entrada");
+        DateTime dataentrada = DateTime.Parse(Console.ReadLine());
+
+        Console.WriteLine("Data saida");
+        DateTime datasaida = DateTime.Parse(Console.ReadLine());
+
+        if (datasaida > dataentrada)
+        {
+
+            lista.Add(new Reservas(numquarto, dataentrada, datasaida));
+        }
+        else
+        {
+            Console.WriteLine("data invalida");
+
+        }
+        Console.WriteLine(.ToString(numquarto));
+    }
+}
